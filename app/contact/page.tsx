717a17b7fd4dce1b2718fcf8bedb34b5752cf0d4
Atagher James <113046523+Atagherjames@users.noreply.github.com>
2024-07-22 09:24:26 +0100

    Contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,13 +1,15 @@
 "use client";
+import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-
 import { Button } from "@/components/ui/button";
+import PhoneInput from "react-phone-number-input";
+import "react-phone-number-input/style.css";
+
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -16,15 +18,41 @@ import {
 import { Input } from "@/components/ui/input";
 
 import ContactHero from "@/components/ContactHero";
-
 import { BiChat, BiHome, BiPhoneIncoming } from "react-icons/bi";
+import { use } from "react";
 
 const contact = () => {
-  // const formSchema = z.object({
-  //   username: z.string().min(2, {
-  //     message: "Username must be at least 2 characters.",
-  //   }),
-  // });
+  const [phoneNumber, setPhoneNUmber] = useState("");
+
+  const formSchema = z.object({
+    firstName: z.string().min(2, {
+      message: "First name must be at least 2 characters.",
+    }),
+
+    lastName: z.string().min(2, {
+      message: "Last name must be at least 2 characters.",
+    }),
+
+    email: z.string().email({ message: "Invalid email address." }),
+    phoneNumber: z.string(),
+  });
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phoneNumber: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
 
   return (
     <>
@@ -75,27 +103,83 @@ const contact = () => {
         </section>
       </div>
       {/* FORM */}
-      {/* <Form {...form}>
-        <form onSubmit={() => {}} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form> */}
+
+      <section className="grid place-items-center">
+        <div className=" p-8 border-2 rounded border-blue-1">
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-8 w-[900px]"
+            >
+              <div className="grid grid-cols-2 gap-5 w-full ">
+                <FormField
+                  control={form.control}
+                  name="firstName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>First Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="First name" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="lastName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Last Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Last name" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Email" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="phoneNumber"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Phone Number</FormLabel>
+                      <FormControl>
+                        <PhoneInput
+                          international
+                          defaultCountry="RU"
+                          className="border-2 p-2 rounded  border-gray-200 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <Button type="submit" className="bg-blue-1 text-white-1">
+                Submit
+              </Button>
+            </form>
+          </Form>
+        </div>
+      </section>
     </>
   );
 };
