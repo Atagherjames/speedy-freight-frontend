2b01f6970fa6fdb72a6ad10018e0ee6ad7f668c7
Atagher James <113046523+Atagherjames@users.noreply.github.com>
2024-07-25 05:39:53 +0100

    update

---
+++ app/bookings/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const page = () => {
+  return (
+    <div className="flex ">
+      <div>
+        {/* spped freight image */}
+        {/* <Image ></Image> */}
+      </div>
+
+      {/* Quote */}
+      <div>{/* quot form */}</div>
+    </div>
+  );
+};
+
+export default page;
--- constants/index.ts
+++ constants/index.ts
@@ -1,34 +1,39 @@
 export const navbarLinks = [
   {
-    label: 'Home',
-    route: '/',
+    label: "Home",
+    route: "/",
   },
   {
-    label: 'About Us',
-    route: '/about',
+    label: "About Us",
+    route: "/about",
   },
 
   {
-    label: 'Our Operations',
-    route: '/our-operations',
+    label: "Our Operations",
+    route: "/our-operations",
   },
   {
-    label: 'Contact',
-    route: '/contact',
+    label: "Contact",
+    route: "/contact",
+  },
+
+  {
+    label: "Get A Quote",
+    route: "/bookings",
   },
 ];
 
 export const sliders = [
   {
-    title: 'one',
-    subtitle: 'Subtitle for one',
+    title: "one",
+    subtitle: "Subtitle for one",
   },
   {
-    title: 'two',
-    subtitle: 'Subtitle for two',
+    title: "two",
+    subtitle: "Subtitle for two",
   },
   {
-    title: 'three',
-    subtitle: 'Subtitle for three',
+    title: "three",
+    subtitle: "Subtitle for three",
   },
 ];
